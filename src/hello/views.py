10b4b55b8cb4c1e887528734f44d7acc77a00b6b
sblancog <sblancog@GUP0075>
2025-11-08 12:56:36 -0600

    fixing api response to be valid json

--- src/hello/views.py
+++ src/hello/views.py
@@ -1,11 +1,12 @@
 
+from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 class HealthView(APIView):
     def get(self, request):
-        return Response({"status": "ok"})
+        return JsonResponse({"status": "ok"})
 
 class HelloView(APIView):
     def get(self, request):
-        return Response({"message": "Hello from Django + Swarm!"})
+        return JsonResponse({"message": "Hello, world!"})
